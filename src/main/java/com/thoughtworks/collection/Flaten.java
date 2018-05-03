ee58735ab48e2c8997baced9c16ad3f3ffedd9c7
chensen <1019812436@qq.com>
2018-05-03 16:58:51 +0800

    FatenTest所有测试用例测试通过

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,7 +2,10 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Flaten {
 
@@ -12,10 +15,32 @@ public class Flaten {
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> res = new ArrayList<>();
+        for(Integer[] arr : array) {
+            for(int num : arr) {
+                res.add(num);
+            }
+        }
+
+        return res;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<>();
+        for(Integer[] arr : array) {
+            for(int num : arr) {
+                list.add(num);
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        for(int num : list) {
+            if(!res.contains(num)) {
+                res.add(num);
+            }
+        }
+
+        return res;
     }
 }
