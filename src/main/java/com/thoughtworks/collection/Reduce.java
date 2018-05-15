79abfe92f08f62f4aa2cd35a484a72f516a0236b
chensen <1019812436@qq.com>
2018-05-15 22:08:48 +0800

    Reduce所有测试用例测试通过

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Reduce {
@@ -13,43 +14,124 @@ public class Reduce {
     }
 
     public int getMaximum() {
-        throw new NotImplementedException();
+        int max = Integer.MIN_VALUE;
+
+        for(int num : arrayList) {
+            if(num > max) {
+                max = num;
+            }
+        }
+        return max;
     }
 
     public double getMinimum() {
-        throw new NotImplementedException();
+        double min = Double.MAX_VALUE;
+
+        for(int num : arrayList) {
+            if(num < min) {
+                min = num;
+            }
+        }
+        return min;
     }
 
     public double getAverage() {
-        throw new NotImplementedException();
+        double average = 0;
+        double sum = 0.0;
+
+        for(int num : arrayList) {
+            sum += num;
+        }
+        average = sum / arrayList.size();
+
+        return average;
     }
 
     public double getOrderedMedian() {
-        throw new NotImplementedException();
+        double mid = 0;
+        int midIndex = (arrayList.size()-1) / 2;
+        if(arrayList.size() % 2 == 0) {
+            mid = (arrayList.get(midIndex) + arrayList.get(midIndex+1)) / 2.0;
+        } else {
+            mid = arrayList.get(midIndex);
+        }
+        return mid;
     }
 
     public int getFirstEven() {
-        throw new NotImplementedException();
+        int firstEven = 0;
+        for(int num : arrayList) {
+            if(num % 2 == 0) {
+                firstEven = num;
+                break;
+            }
+        }
+        return firstEven;
     }
 
     public int getIndexOfFirstEven() {
-        throw new NotImplementedException();
+        int firstEvenIndex = 0;
+        for(int i=0; i<arrayList.size(); i++) {
+            if(arrayList.get(i) % 2 == 0) {
+                firstEvenIndex = i;
+                break;
+            }
+        }
+        return firstEvenIndex;
     }
 
     public boolean isEqual(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        if(this.arrayList.size() != arrayList.size()) {
+            return false;
+        }
+        boolean res = true;
+        for(int i=0; i<arrayList.size(); i++) {
+            if(this.arrayList.get(i) != arrayList.get(i)) {
+                res = false;
+                break;
+            }
+        }
+        return res;
     }
 
     //实现接口SingleLink，然后再此函数内使用
     public Double getMedianInLinkList(SingleLink singleLink) {
-        throw new NotImplementedException();
+        double mid = 0;
+
+        for(int num : arrayList) {
+            singleLink.addTailPointer(num);
+        }
+        int size = arrayList.size();
+        int midIndex = (arrayList.size()-1) / 2;
+        if(size % 2 == 0) {
+            int x1 = (int) singleLink.getNode(midIndex+1);
+            int x2 = (int) singleLink.getNode(midIndex+2);
+            mid = (x1 + x2) / 2.0;
+        } else {
+            mid = (double) singleLink.getNode(midIndex);
+        }
+
+
+        return mid;
     }
 
     public int getLastOdd() {
-        throw new NotImplementedException();
+        int lastOdd = -1;
+        for(int num : arrayList) {
+            if(num % 2 != 0) {
+                lastOdd = num;
+            }
+        }
+        return lastOdd;
     }
 
     public int getIndexOfLastOdd() {
-        throw new NotImplementedException();
+        int lastOddIndex = -1;
+        for(int i=0; i<arrayList.size(); i++) {
+            if(arrayList.get(i) % 2 != 0) {
+                lastOddIndex = i;
+            }
+        }
+        return lastOddIndex;
     }
 }
--- src/main/java/com/thoughtworks/collection/SingleLink.java
+++ src/main/java/com/thoughtworks/collection/SingleLink.java
@@ -1,13 +1,67 @@
 package com.thoughtworks.collection;
+//
+//public interface SingleLink<T> {
+//    public T getHeaderData();
+//    public T getTailData();
+//    public int size();
+//    public boolean isEmpty();
+//    public boolean deleteFirst();
+//    public boolean deleteLast();                //删除尾部元素；
+//    public void addHeadPointer(T item);    //添加头指针
+//    public void addTailPointer(T item);         //添加尾指针
+//    public T getNode(int index);
+//}
 
-public interface SingleLink<T> {
-    public T getHeaderData();
-    public T getTailData();
-    public int size();
-    public boolean isEmpty();
-    public boolean deleteFirst();
-    public boolean deleteLast();                //删除尾部元素；
-    public void addHeadPointer(T item);    //添加头指针
-    public void addTailPointer(T item);         //添加尾指针
-    public T getNode(int index);
+import java.util.LinkedList;
+import java.util.List;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class SingleLink<T>{
+    List<T> list = new LinkedList<T>();
+
+    public T getHeaderData() {
+        return list.get(0);
+    }
+
+    public T getTailData() {
+        return list.get(list.size()-1);
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public boolean deleteFirst() {
+        if(list.size() < 1) {
+            return false;
+        }
+        list.remove(0);
+        return true;
+    }
+
+    public boolean deleteLast() {
+        if(list.size() < 1) {
+            return false;
+        }
+        list.remove(list.size()-1);
+        return true;
+    }
+
+    public void addHeadPointer(T item) {
+        list.add(0, item);
+    }
+
+    public void addTailPointer(T item) {
+        list.add(item);
+    }
+
+    public T getNode(int index) {
+        return list.get(index);
+    }
 }
